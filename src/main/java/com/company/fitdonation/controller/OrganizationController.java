7223fe4fbe1e7a1089a198926385066b0158ba84
Kirill Krainov-Timanovsky <79582042+kirillkrainov95@users.noreply.github.com>
2023-10-01 13:28:23 +0300

    changes

--- src/main/java/com/company/fitdonation/controller/OrganizationController.java
+++ src/main/java/com/company/fitdonation/controller/OrganizationController.java
@@ -1,2 +1,27 @@
-package com.company.fitdonation.controller;public class OrganizationController {
+package com.company.fitdonation.controller;
+
+import com.company.fitdonation.dto.OrganizationDto;
+import com.company.fitdonation.dto.UserDto;
+import com.company.fitdonation.repository.OrganizationRepository;
+import com.company.fitdonation.service.OrganizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/organizations")
+@RequiredArgsConstructor
+public class OrganizationController {
+
+    private final OrganizationService organizationService;
+
+    @GetMapping
+    public ResponseEntity<List<OrganizationDto>> getAllOrganizations() {
+        return ResponseEntity.ok(organizationService.getAllOrganizations());
+    }
+
 }
--- src/main/java/com/company/fitdonation/service/OrganizationService.java
+++ src/main/java/com/company/fitdonation/service/OrganizationService.java
@@ -1,2 +1,29 @@
-package com.company.fitdonation.service;public class OrganizationService {
+package com.company.fitdonation.service;
+
+import com.company.fitdonation.dto.OrganizationDto;
+import com.company.fitdonation.dto.UserDto;
+import com.company.fitdonation.entity.Organization;
+import com.company.fitdonation.entity.User;
+import com.company.fitdonation.mapper.OrganizationMapper;
+import com.company.fitdonation.mapper.UserMapper;
+import com.company.fitdonation.repository.OrganizationRepository;
+import com.company.fitdonation.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class OrganizationService {
+
+    private final OrganizationRepository organizationRepository;
+    private final OrganizationMapper organizationMapper;
+
+    public List<OrganizationDto> getAllOrganizations() {
+        return organizationRepository.findAll().stream()
+                .map(organizationMapper::toDto)
+                .collect(Collectors.toList());
+    }
 }
